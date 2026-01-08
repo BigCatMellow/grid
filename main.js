906b70da809e724d8024a09781cb0218e9f0e40f
BigCatMellow <goldenjanitors@gmail.com>
2026-01-08 10:02:03 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 app.whenReady().then(() => {
     const win = new BrowserWindow({
-        width: 250,
-        height: 280,
+        width: 220,
+        height: 250,
         frame: false,
         alwaysOnTop: true,
-        transparent: true,
-        resizable: true
+        resizable: true,
+        skipTaskbar: false,
+        webPreferences: {
+            nodeIntegration: false
+        }
     });
+    
     win.loadFile('sudoku_discreet.html');
+    
+    // Drag window by clicking anywhere
+    win.setMovable(true);
+});
+
+app.on('window-all-closed', () => {
+    app.quit();
 });
